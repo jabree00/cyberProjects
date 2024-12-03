82543ac0b59e36e2fd797ec462ace4ee9ab185e1
jabree00 <103905098+jabree00@users.noreply.github.com>
2024-12-02 23:46:07 -0500

    Update pstree_rev.sh
    Added a comment to describe the importance of this code.

--- pstree_rev.sh
+++ pstree_rev.sh
@@ -1,9 +1,17 @@
 #!/bin/sh 
 
-echo "What process name do you want to search for?"
+# This is an (unfinished) project idea to find the PID of the parent(s)/ancestors(s)
+# of the processes' that I am working with. This project c
 
+# Grab process name from user 
+echo "What process name do you want to search for?"
 read processName
 
+# Get a list of process IDs linked to that name
 pids=`ps -e | grep $processName | awk '{print $1)'`
 
-while read pids  
+# Convert this output into a list 
+
+
+# Iterate through each PID in the list, 
+# Recursively, print the PID of each parent
