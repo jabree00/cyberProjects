a40c22b58f1344ca73e422e5ad8658caec84a2e4
jabree00 <103905098+jabree00@users.noreply.github.com>
2024-12-03 00:23:56 -0500

    Add files via upload

---
+++ permissions.sh
@@ -0,0 +1,62 @@
+vi /etc/group
+sudo groupadd developer
+useradd alice 
+sudo useradd alice
+id alice
+id 
+ls -l /etc/hosts
+ls -l /home
+groupadd developers
+sudo groupadd developer
+newgrp developers
+newgrp developer
+sudo newgrp developer
+id
+groupadd admin
+sudo groupadd admin
+newgrp developer
+newgrp admin
+sudo newgrp developer
+id
+sudo newgrp developer
+groups bob
+useradd bob
+sudo useradd bob
+groups bob
+touch report.txt
+sudo groupadd staff
+sudo chgrp staff report.txt
+ls -l report.txt
+mkdir data
+sudo groupadd team 
+sudo chgrp team data 
+ls -l data
+ls -l data/
+ls -l ./
+ls -l ./data
+ls -l ./data/
+mkdir projects 
+chmod 750 projects
+touch notes.txt
+chmod 640 notes.txt
+umask 
+umask 027 
+touch newfile.txt
+ls -l newfile.txt 
+history > permissions.sh
+vim permissions.sh
+history
+:q!
+history > hist.txt
+sed -e '1,784' hist.txt < hist.txt
+sed -e '1,784d' hist.txt < hist.txt
+cut -c 8- hist.txt > hist.txt
+cat hist.txt
+sed -e '1,784d' hist.txt < hist.txt
+cat hist.txt
+sed -e '1,784d' hist.txt < hist2.txt
+ls
+hsitory > hist.txt
+history > hist.txt
+sed -e '1,784d' hist.txt > hist.txt
+cat hist.txt
