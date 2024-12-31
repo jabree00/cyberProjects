8619361bf91d1c904a524db7ad267d7872f9ba8e
jabree00 <103905098+jabree00@users.noreply.github.com>
2024-12-31 16:06:29 -0500

    Create indexViewer.py
    This program takes a string as input and displayed a formatted out of the characters with the indices written above. 
    
    TO DO:
    - align all the characters using the correct padding 
    - support input of any length 
    - add validation

---
+++ scripts/python/indexViewer.py
@@ -0,0 +1,34 @@
+#Give a number of spaces, this function creates padding of that length 
+def paddingMaker(spaces):
+	return " " * spaces 
+	
+print("WELCOME TO INDEX VIEWER")
+print("This app displays the char indices of some given text. ")
+
+text = input("Type some text. The character limit is 25:\n")
+text = text.upper()
+
+#Add some vertical space 
+print()
+
+#Remove the case that the text length is 0
+if (len(text) == 0):
+	print("Have a nice day!")
+	exit(0)
+
+def printIndexedLetters(inText):
+	#Print the indices 
+	for i in range(len(inText)):
+		#check if i is less than text length
+		if i < (len(inText) - 1):
+			print(str(i) + paddingMaker(len(str(i)) + 1), end="")
+		else:
+			print(str(i) + paddingMaker(len(str(i)) + 1), end="\n")
+
+	#print each letter with the padding after it 
+	for i in range(len(text)):
+		letter = text[i:i+1]
+		print(letter + paddingMaker(len(str(i)) + 1),end="")
+
+printIndexedLetters(text)
+print()
