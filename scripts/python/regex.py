823ac4fc42d0f51df9b47d3469297e208bc65394
jabree00 <103905098+jabree00@users.noreply.github.com>
2024-12-31 16:20:05 -0500

    Create regex.py
    A quick mind dump of a flexible regex script. Has not been error-checked yet, so might be buggy.

---
+++ scripts/python/regex.py
@@ -0,0 +1,27 @@
+"""The + sign matches at least one of the previous char or group of characters 
+The * sign matches 0 or more of the previous char or group of character 
+Special symbols include... 
+\w which means any character 
+
+The re module is helpful for regular expressions 
+The findall method in the re module returns a list of all matches in the input string. 
+"""
+
+import re 
+
+filename = input("Type the name of the file that you want to work with") 
+
+try: 
+  text = ""
+  with open("filename","r") as file: 
+    text = file.readAll()
+    
+except FileNotFoundError:
+  print("This file does not exist. Check spelling.") 
+  exit()
+
+
+pattern = input("Type your regex pattern") 
+matches = re.findAll(pattern,text)
+print(matches)
+  
