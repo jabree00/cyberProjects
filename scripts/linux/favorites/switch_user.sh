57cbcd6809cefd4ec7d62206749f29ae07822d33
jabree00 <103905098+jabree00@users.noreply.github.com>
2025-01-01 12:02:01 -0500

    Update and rename scripts/switch_user.sh to scripts/linux/favorites/switch_user.sh
    Added a header comment to describe overall purpose 
    
    Remove confusing, unclear comments at the bottom of the script

---
+++ scripts/linux/favorites/switch_user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+#A simple program to demonstrate switching to a new user. 
+
+echo "Which user would you like to sign in as?"
+read userin 
+
+echo "Running the su $userIn command by prepared to type a password. 
+su $userIn 
+
