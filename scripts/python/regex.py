7b3e9e10fb7c7508ab42041dc4c7b8f2a23bf3b8
jabree00 <103905098+jabree00@users.noreply.github.com>
2024-12-31 20:35:51 -0500

    Add files via upload

--- scripts/python/regex.py
+++ scripts/python/regex.py
@@ -1,13 +1,13 @@
 """The + sign matches at least one of the previous char or group of characters 
 The * sign matches 0 or more of the previous char or group of character 
 Special symbols include... 
-\w which means any alphanumeric character and the underscore. 
+\w which means any character (including whitespace) 
 \d which means any digit 
-\s which means a single whitespace character
-\. which means a literal . symbol
+\s which means a series of non-whitespace characters
 
 The re module is helpful for regular expressions 
 The findall method in the re module returns a list of all matches in the input string. 
+
 """
 
 import re 
@@ -16,15 +16,14 @@ filename = input("Type the name of the file that you want to work with:\n")
 
 try: 
   text = ""
-  with open("filename","r") as file: 
-    text = file.readAll()
+  with open(filename,"r") as file: 
+    text = file.read()
     
 except FileNotFoundError:
   print("This file does not exist. Check spelling.") 
   exit()
 
 
-pattern = input("Type your regex pattern") 
-matches = re.findAll(pattern,text)
+pattern = input("Type your regex pattern.\nExample: \w*e\s to find words that end with e:\n") 
+matches = re.findall(pattern,text)
 print(matches)
-  
